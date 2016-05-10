ffcc4451d7147bd0b5609245c9131711af22aa15
Mike <ccnp_pro@yahoo.com>
2016-05-10 01:06:03 -0400

    Add more transponders, also make changing transponder id easier

--- CoreIR.ino
+++ CoreIR.ino
@@ -1,23 +1,77 @@
- /*
- * 
- */
+//****CONFIGURATION OPTIONS****
+//change transponder ID # by setting a different transponder number
+//available values are 1-6
 
+#define transponder1
 
+//****CONFIGURATION END****
+
+//setup for the transponder IR signal
 #include <IRremote.h>
-
 IRsend irsend;
-  int khz = 15; // 38kHz carrier frequency for the NEC protocol
- //6491857 unsigned int rawData[33] = {22, 22, 48, 22, 88, 22, 22, 22, 48, 22, 88, 22, 48, 48, 22, 48, 22, 48, 22, 68, 48, 93, 93, 22, 22, 22, 22, 53, 48, 48, 48, 22, 22};
- unsigned int rawData[35] = {22, 48, 22, 22, 93, 22, 48, 68, 48, 68, 22, 48, 22, 22, 68, 48, 22, 68, 22, 22, 22, 93, 22, 93, 22, 22, 22, 48, 22, 22, 22, 22, 48, 22, 22};
+int khz = 15;
 
+//setup for blinking the arduino LED
+const int ledPin =  13; 
+int ledState = LOW;
+unsigned long previousMillis = 0;
+const long interval = 100;
+
+//transponder defines
+#ifdef transponder1
+    //raw on/off for code 5196502
+    unsigned int rawData[33] = {48, 22, 22, 22, 98, 22, 68, 22, 48, 48, 22, 22, 48, 48, 48, 98, 48, 22, 22, 48, 48, 48, 22, 22, 22, 178, 22, 22, 22, 68, 22, 22, 22};
+    int size = 33;
+#endif
+#ifdef transponder2 
+    //2889174
+    unsigned int rawData[35] = {22, 22, 22, 48, 98, 22, 22, 22, 22, 22, 48, 48, 22, 22, 22, 68, 22, 68, 22, 22, 48, 68, 22, 48, 22, 22, 22, 48, 22, 22, 68, 48, 48, 103, 83};
+    int size = 35;
+#endif
+#ifdef transponder3 
+    //3904041
+    unsigned int rawData[29] = {68, 48, 98, 22, 22, 22, 22, 103, 48, 22, 22, 103, 22, 48, 22, 22, 22, 150, 22, 48, 48, 126, 22, 48, 22, 22, 22, 48, 22};
+    int size = 29;
+#endif
+#ifdef transponder4 
+    //6491857
+    unsigned int rawData[33] = {22, 22, 48, 22, 88, 22, 22, 22, 48, 22, 88, 22, 48, 48, 22, 48, 22, 48, 22, 68, 48, 98, 98, 22, 22, 22, 22, 53, 48, 48, 48, 22, 22};
+    int size = 33;
+#endif
+#ifdef transponder5
+    //3852671
+    unsigned int rawData[31] = {68, 48, 98, 22, 68, 22, 68, 68, 48, 22, 22, 103, 22, 22, 22, 22, 48, 48, 22, 73, 48, 76, 68, 48, 22, 22, 48, 22, 48, 22, 22};
+    int size = 31;
+#endif
+#ifdef transponder6
+    //4742860
+    unsigned int rawData[35] = {22, 48, 22, 22, 88, 22, 48, 68, 48, 68, 22, 48, 22, 22, 68, 48, 22, 68, 22, 22, 22, 88, 22, 88, 22, 22, 22, 52, 22, 22, 22, 22, 48, 22, 22};
+    int size = 35;
+#endif
 void setup()
 {
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  //setup for blinking the arduino LED
+  pinMode(ledPin, OUTPUT);    //set transponder raw id
 }
 
 void loop() {
-  //6491857 irsend.sendRaw(rawData, 33, khz); //Note the approach used to automatically calculate the size of the array.
-  irsend.sendRaw(rawData, 35, khz); //Note the approach used to automatically calculate the size of the array.
-  delayMicroseconds(480);
+  irsend.sendRaw(rawData, size, khz);
+  delayMicroseconds(2600);
+
+  //blink the LED if applicable
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
 }
